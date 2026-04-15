b40177549034be65045a6c738441aaf2e2002a2e
Marto-kun <martinvozme456@gmail.com>
2026-04-15 08:03:19 -0500

    Metodos de impresion de informacion e implementacion del menu

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-
+        int opc;
         Sistema sis = new Sistema();
-
+        System.out.println("Crear el perfil medico del paciente: ");
         PerfilMedico pm1 = sis.crearPerfilMedico();
 
-
-
-
+        System.out.println("Crear la factura: ");
         Factura fac = sis.crearFactura();
+
+        opc = Sistema.menu();
+
+        switch (opc){
+            case 1:
+                //Imprimir Info Vehiculo
+                sis.imprimirInfoMedico(v1);
+                break;
+
+            case 2:
+                //Actualizar Info Vehiculo
+                sis.imp(v1);
+                break;
+
+            case 3:
+                //Imprimir la aceleracion del vehiculo
+                sis.imprimirAceleracion(v1);
+                break;
+
+            case 4:
+                //Imprimir la edad del dueño
+                sis.imprirEdadDuenio(v1);
+                break;
+        }
 
 
 
--- src/Sistema.java
+++ src/Sistema.java
@@ -60,14 +60,17 @@ public class Sistema {
         System.out.println("Edad: "+ pm.calcularEdad());
         System.out.println("Frecuencia caridaca maxima: "+pm.calcularFreCardiacaMaxima());
 
-
+        System.out.println("IMC: "+pm.calcularIMC());
+        System.out.println("Tabla de IMC: ");
+        System.out.println("VALORES DE IMC:\n Bajo peso: menos de 18.5 " +
+                "Normal: entre 18.5 y 24.9 Sobrepeso: entre 25 y 29.9 Obeso: 30 o más");
 
     }
 
 
     public Factura crearFactura(){
 
-        System.out.println("Ingrese los datos del paciente: ");
+        System.out.println("Ingrese los datos de la pieza: ");
         System.out.print("Numero de pieza: ");
         String numPieza = sc.next();
 
@@ -82,9 +85,33 @@ public class Sistema {
 
         Factura fac = new Factura(numPieza, descripcionPieza, cantidadProducto, precio);
 
-
         return fac;
     }
 
+    public void imprimirInfoFactura(Factura fac){
+        System.out.println("---- Datos de la Factura ----");
+        System.out.println("Numero de pieza: "+fac.getNumPieza());
+        System.out.println("Descripcion de la Pieza: "+fac.getDescripcionPieza());
+        System.out.println("Cantidad: "+fac.getCantidadProducto());
+        System.out.println("Precio: "+fac.getPrecio());
+
+
+    }
+
+    //Metodo de menu
+
+    public static int menu() {
+        Scanner sc = new Scanner(System.in);
+        int opc;
+        System.out.println("---- Menu Principal ----");
+        System.out.println("Ingrese la opcion deseada: ");
+        System.out.println("1) Imprimir Informacion del Paciente");
+        System.out.println("2) Imprimir Informacion de la Factura");
+        System.out.println("3) Salir");
+        System.out.print(">>> ");
+        opc = sc.nextInt();
+        return opc;
+
+    }
 
 }
